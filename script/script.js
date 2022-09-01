5ca42a78799096a68eb3f1b5470a509ba84f2c0d
Luiz Alexsandro <100519613+Luizalexsandro99@users.noreply.github.com>
2022-09-01 15:00:57 -0300

    Atualicações paginas, links e seções

--- script/script.js
+++ script/script.js
@@ -1,13 +1,14 @@
 const aparecerPrimeiro = document.querySelector('#aparecerPrimeiro')
 const aparecerDepois = document.querySelector('#aparecerDepois')
 
-setTimeout(()=>{
-    aparecerPrimeiro.classList.add('transformeIndo')
-}, 5000)
 
-setTimeout(()=>{
+/* setTimeout(()=>{
+    aparecerPrimeiro.classList.add('transformeIndo')
+}, 5000) */
+
+/* setTimeout(()=>{
     aparecerDepois.style.display = 'block'
     aparecerDepois.classList.add('aparecendo')
-}, 5000)
+}, 5000) */
 
 
